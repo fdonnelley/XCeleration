c74bc4a393bf218362c08c593dcb4fed82b6c669
Teo <teodonnelley@gmail.com>
2025-01-20 17:17:44 -0800

    added in wireless connectivity starting code

--- lib/screens/bib_number_screen.dart
+++ lib/screens/bib_number_screen.dart
@@ -6,8 +6,6 @@ import 'dart:convert';
 import 'test_camera.dart';
 import '../database_helper.dart';
 // import 'package:camera/camera.dart';
-// import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-// import 'package:race_timing_app/bluetooth_service.dart' as app_bluetooth;
 
 class BibNumberScreen extends StatefulWidget {
   const BibNumberScreen({super.key});
@@ -16,15 +14,13 @@ class BibNumberScreen extends StatefulWidget {
   State<BibNumberScreen> createState() => _BibNumberScreenState();
 }
 
+
 class _BibNumberScreenState extends State<BibNumberScreen> {
   final List<TextEditingController> _controllers = [];
   final List<FocusNode> _focusNodes = [];
   final List<Map<String, dynamic>> _bibRecords = [];
   // final ImagePicker _imagePicker = ImagePicker();
   // late CameraController _cameraController;
-  // final app_bluetooth.BluetoothService _bluetoothService = app_bluetooth.BluetoothService();
-  // BluetoothDevice? _connectedDevice;
-  // List<BluetoothDevice> _availableDevices = [];
 
   void _addBibNumber([String bibNumber = '', List<double>? confidences = const [], XFile? image]) async {
     final index = _bibRecords.length;
@@ -53,20 +49,7 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
           });
         }
       }
-
-      final runner = await DatabaseHelper.instance.getRaceRunnerByBib(1, bibNumber, getShared: true);
-      if (runner[0] == null) {
-        setState(() {
-          _bibRecords[index]['flags']['not_in_database'] = true;
-        });
-      }
-      else {
-        setState(() {
-          _bibRecords[index]['name'] = runner[0]['name'];
-          _bibRecords[index]['school'] = runner[0]['school'];
-        });
-      }
-      _flagBibNumberDuplicates(bibNumber);
+      flagBibNumber(index, bibNumber);  
     }
     else {
       // Automatically focus the last input box
@@ -76,41 +59,8 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
     }
   }
 
-  void _flagBibNumberDuplicates(String bibNumber, {bool removeFlags = false}) {
-    List duplicates = [];
-    for (int i = 0; i < _bibRecords.length; i++) {
-      if (_bibRecords[i]['bib_number'] == bibNumber) {
-        duplicates.add(i);
-      }
-    }
-    if (removeFlags) {
-      if (duplicates.length == 1) {
-        setState(() {
-          _bibRecords[duplicates[0]]['flags']['duplicate_bib_number'] = false;
-        });
-      }
-    }
-    else {
-      for (int i = 0; i < duplicates.length; i++) {
-        if (duplicates.length >= 2) {
-          setState(() {
-            _bibRecords[duplicates[i]]['flags']['duplicate_bib_number'] = true;
-          });
-        }
-      }
-    }
-  }
-    
-  void _updateBibNumber(int index, String bibNumber) async {
-    final oldBibNumber = _bibRecords[index]['bib_number'];
-    setState(() {
-      _bibRecords[index]['bib_number'] = bibNumber;
-    });
-    _flagBibNumberDuplicates(oldBibNumber, removeFlags: true);
-    if (bibNumber.isNotEmpty) {
-      _bibRecords[index]['flags']['duplicate_bib_number'] = false;
-      _flagBibNumberDuplicates(bibNumber);
-      final runner = await DatabaseHelper.instance.getRaceRunnerByBib(1, bibNumber, getShared: true);
+  void flagBibNumber(int index, String bibNumber) async {
+    final runner = await DatabaseHelper.instance.getRaceRunnerByBib(1, bibNumber, getShared: true);
       if (runner[0] == null) {
         setState(() {
           _bibRecords[index]['flags']['not_in_database'] = true;
@@ -120,9 +70,23 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
         setState(() {
           _bibRecords[index]['name'] = runner[0]['name'];
           _bibRecords[index]['school'] = runner[0]['school'];
-          _bibRecords[index]['flags']['not_in_database'] = false;
+           _bibRecords[index]['flags']['not_in_database'] = false;
         });
       }
+      _bibRecords[index]['flags']['duplicate_bib_number'] = false;
+      for (int i = 0; i < _bibRecords.length; i++) {
+        if (i != index && _bibRecords[i]['bib_number'] == bibNumber) {
+          _bibRecords[index]['flags']['duplicate_bib_number'] = true;
+        }
+      }
+  }
+    
+  void _updateBibNumber(int index, String bibNumber) async {
+    setState(() {
+      _bibRecords[index]['bib_number'] = bibNumber;
+    });
+    if (bibNumber.isNotEmpty) {
+      flagBibNumber(index, bibNumber);
     } else {
       setState(() {
         _bibRecords[index]['flags'] = {
@@ -176,13 +140,11 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
   }
 
   Future<void> _deleteBibNumber(int index) async {
-    final bibNumber = _bibRecords[index]['bib_number'];
     setState(() {
       _controllers.removeAt(index);
       _focusNodes.removeAt(index);
       _bibRecords.removeAt(index);
     }); 
-    _flagBibNumberDuplicates(bibNumber, removeFlags: true);
   }
 
 
@@ -250,120 +212,6 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
   String _generateQrData() {
     return jsonEncode(_bibRecords.map((record) => record['bib_number']).toList());
   }
-
-  // void _findDevices() async {
-  //   try {
-  //     // Check if Bluetooth is enabled
-  //     final isOn = await _bluetoothService.isBluetoothOn();
-  //     if (!isOn) {
-  //       _showBluetoothOffDialog();
-  //       return;
-  //     }
-
-  //     // Proceed to find devices if Bluetooth is on
-  //     await for (final devices in _bluetoothService.getAvailableDevices()) {
-  //       setState(() {
-  //         _availableDevices = devices;
-  //       });
-  //     }
-  //   } catch (e) {
-  //     // Handle other exceptions (optional)
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Failed to scan for devices: $e')),
-  //     );
-  //   }
-  // }
-
-  // void _disconnectDevice() async {
-  //   final bluetoothService = app_bluetooth.BluetoothService();
-  //   if (_connectedDevice == null) return;
-
-  //   final confirm = await showDialog<bool>(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         title: const Text('Confirm Disconnect'),
-  //         content: const Text('Are you sure you want to disconnect?'),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             child: const Text('Cancel'),
-  //             onPressed: () => Navigator.of(context).pop(false),
-  //           ),
-  //           TextButton(
-  //             child: const Text('Disconnect'),
-  //             onPressed: () => Navigator.of(context).pop(true),
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-
-  //   if (confirm == true) {
-  //     try {
-  //       await bluetoothService.disconnectDevice(_connectedDevice!);
-  //       setState(() {
-  //         _connectedDevice = null; // Clear the connected device
-  //       });
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(content: Text('Disconnected successfully')),
-  //       );
-  //     } catch (e) {
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text('Failed to disconnect: $e')),
-  //       );
-  //     }
-  //   }
-  // }
-
-  // void _connectToDevice(BluetoothDevice device) async {
-  //   try {
-  //     final connectedDevice = await _bluetoothService.connectToDevice(device);
-  //     setState(() {
-  //       _connectedDevice = connectedDevice;
-  //     });
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text('Connected successfully')),
-  //     );
-  //   } catch (e) {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Failed to connect: $e')),
-  //     );
-  //   }
-  // }
-
-  // void _sendBibNumbers() async {
-  //   if (_connectedDevice == null) return;
-
-  //   try {
-  //     final data = _bibRecords.join(';').codeUnits; // Serialize the bib numbers
-  //     await _bluetoothService.sendData(_connectedDevice!, data);
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text('Bib numbers sent successfully')),
-  //     );
-  //   } catch (e) {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Failed to send bib numbers: $e')),
-  //     );
-  //   }
-  // }
-
-  // void _showBluetoothOffDialog() {
-  //   showDialog(
-  //     context: context,
-  //     builder: (context) => AlertDialog(
-  //       title: const Text('Bluetooth Off'),
-  //       content: const Text('Please turn on Bluetooth to search for devices.'),
-  //       actions: [
-  //         TextButton(
-  //           onPressed: () {
-  //             Navigator.of(context).pop();
-  //           },
-  //           child: const Text('OK'),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 
 
   @override
@@ -488,35 +336,6 @@ class _BibNumberScreenState extends State<BibNumberScreen> {
                 child: const Text('Add Bib Number', style: TextStyle(fontSize: 20)),
               ),
             ),
-            // ElevatedButton(
-            //   onPressed: _findDevices,
-            //   child: const Text('Find Devices'),
-            // ),
-            // if (_connectedDevice == null)
-            //   DropdownButton<BluetoothDevice>(
-            //     hint: const Text('Select Device'),
-            //     items: _availableDevices.map((device) {
-            //       return DropdownMenuItem(
-            //         value: device,
-            //         child: Text(device.platformName),
-            //       );
-            //     }).toList(),
-            //     onChanged: (device) {
-            //       if (device != null) _connectToDevice(device);
-            //     },
-            //   ),
-            // if (_connectedDevice != null)
-            //   ElevatedButton(
-            //     onPressed: _sendBibNumbers,
-            //     child: const Text('Send Bib Numbers'),
-            //   ),
-            // if (_connectedDevice != null) ...[
-            //     const SizedBox(height: 8),
-            //     ElevatedButton(
-            //       onPressed: _disconnectDevice, // Add disconnect button
-            //       child: const Text('Disconnect'),
-            //     ),
-            // ],
           ],
         ),
       ),
