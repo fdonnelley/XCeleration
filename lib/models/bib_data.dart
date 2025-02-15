e3cba90bc22fda2ef19efb9eb49cb3327242b602
Teo <teodonnelley@gmail.com>
2025-02-15 13:50:30 -0800

    updated bib_data to help bib_number_screen to be more simple

--- lib/models/bib_data.dart
+++ lib/models/bib_data.dart
@@ -26,15 +26,27 @@ class BibRecordsProvider with ChangeNotifier {
   final List<BibRecord> _bibRecords = [];
   final List<TextEditingController> _controllers = [];
   final List<FocusNode> _focusNodes = [];
+  bool _isKeyboardVisible = false;
 
   List<BibRecord> get bibRecords => _bibRecords;
   List<TextEditingController> get controllers => _controllers;
   List<FocusNode> get focusNodes => _focusNodes;
+  bool get isKeyboardVisible => _isKeyboardVisible;
 
   void addBibRecord(BibRecord record) {
     _bibRecords.add(record);
-    _controllers.add(TextEditingController(text: record.bibNumber));
-    _focusNodes.add(FocusNode());
+    final controller = TextEditingController(text: record.bibNumber);
+    _controllers.add(controller);
+    
+    final focusNode = FocusNode();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus != _isKeyboardVisible) {
+        _isKeyboardVisible = focusNode.hasFocus;
+        notifyListeners();
+      }
+    });
+    _focusNodes.add(focusNode);
+    
     notifyListeners();
   }
 
@@ -45,14 +57,22 @@ class BibRecordsProvider with ChangeNotifier {
 
   void removeBibRecord(int index) {
     _bibRecords.removeAt(index);
+    _controllers[index].dispose();
     _controllers.removeAt(index);
+    _focusNodes[index].dispose();
     _focusNodes.removeAt(index);
     notifyListeners();
   }
 
   void clearBibRecords() {
     _bibRecords.clear();
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
     _controllers.clear();
+    for (var node in _focusNodes) {
+      node.dispose();
+    }
     _focusNodes.clear();
     notifyListeners();
   }
